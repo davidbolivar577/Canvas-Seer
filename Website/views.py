d1ae15042a3e25fcdf4b6d68f85f62de80802a8e
Nussr2 <topsecretorginization@gmail.com>
2025-10-25 12:44:00 -0600

    Hope this doesn't break anything

--- Website/views.py
+++ Website/views.py
@@ -1,7 +1,40 @@
 from flask import Blueprint, render_template, redirect, url_for
-
+from flask_login import login_required, current_user
+import json
 views = Blueprint('views', __name__)
 
+login_status = False
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST': 
+        note = request.form.get('note')#Gets the note from the HTML 
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error') 
+        else:
+            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
+            db.session.add(new_note) #adding the note to the database 
+            db.session.commit()
+            flash('Note added!', category='success')
+            login_status = True
+
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():  
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
+'''
 @views.route('/')
 def root():
     return redirect(url_for('views.home'))
@@ -9,9 +42,10 @@ def root():
 @views.route('/home')
 def home():
     return render_template("home.html")
+'''
 
 @views.route('/login')
-def login(login_status):
+def login():
     #if logged in
     if (login_status):
         return redirect(url_for('views.home'))
@@ -23,9 +57,9 @@ def signup():
     return render_template("sign_up.html")
 
 @views.route('/logout')
-def logout(login_status):
-    if (login_status):
-        return redirect(url_for('views.home'))
+def logout():
+    if (login_status == False):
+        return redirect(url_for('login_page.home'))
     return render_template("logout.html")
 
 @views.route('/settings')
@@ -59,3 +93,5 @@ def course(course):
 @views.route('/courses/<course>/<module>')
 def module(course, module):
     return render_template("module.html", path = [course, module])
+
+
