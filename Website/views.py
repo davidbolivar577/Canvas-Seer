2ffd38dd9aa206f20af2add8b67fd76e5040463a
Nussr2 <topsecretorginization@gmail.com>
2025-10-25 00:33:51 -0600

    Fixing view.home

--- Website/views.py
+++ Website/views.py
@@ -3,12 +3,12 @@ from flask import Blueprint, render_template, redirect, url_for
 views = Blueprint('views', __name__)
 
 @views.route('/')
-def home():
-    return redirect(url_for('/home'))
+def root():
+    return redirect(url_for('views.home'))
 
 #todo #6
 @views.route('/home')
-def home2():
+def home():
     return render_template("home.html")
 
 @views.route('/login')
