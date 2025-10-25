014925351d7c704a1830f668a3333102d92ad08c
EthanFromTexas <ecotter2020@gmail.com>
2025-10-24 23:44:09 -0600

    lmao

--- Website/views.py
+++ Website/views.py
@@ -1,7 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/home')
+def home2():
+    return redirect(url_for('/'))
+
+"""
+@views.route('/home')
+def home2():
+    return render_template("home_2.html")
+
+"""
