7d64d9e06237eb7baf8c4484ff6451c1ceb5a0c2
EthanFromTexas <ecotter2020@gmail.com>
2025-10-24 18:54:37 -0600

    More setup

---
+++ Website/auth.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/Logout')
+def logout():
+    return "<p>Logout</p>"
+
