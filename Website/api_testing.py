659c24f3d04c41e07ed1cc051cd2ceacd113a481
Carter <beauduke2014@gmail.com>
2025-10-25 11:32:57 -0600

    merging files to get us all on same page.

---
+++ Website/api_testing.py
@@ -0,0 +1,26 @@
+'''
+import requests
+import subprocess
+
+#debug
+import pprint
+
+
+base_url = "https://canvas.instructure.com/api/v1/courses"
+
+#curl -H "Authorization: Bearer <ACCESS-TOKEN>" "https://canvas.instructure.com/api/v1/courses"
+
+def get_grade_info():
+ #   url = f"{base_url}/users?/{name}"
+
+    headers = {"Authorization": "Bearer 10706~wJETeETFe6nwyHhTkDkGnth8yBYrWmKyzP438Qnww3h2hWknDHCMextVf8X4KTrU"}
+    response = requests.get("https://canvas.instructure.com/api/v1/courses", headers=headers)
+
+    if response.status_code == 200:
+        test_data = response.json()
+        pprint.pprint(test_data) 
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+
+get_grade_info()
+'''
